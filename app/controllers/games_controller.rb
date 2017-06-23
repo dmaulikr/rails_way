82eca4a143cf17a1b14031f28e234cc6a63d60e3
Lucas Caton <lucascaton@gmail.com>
2017-06-23 21:00:56 +1000

    Rename images

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,8 @@ class GamesController < ApplicationController
   end
 
   def show
+    @images = Dir[Rails.root.join 'app', 'assets', 'images', 'railsroads', '*'].map do |file|
+      "railsroads/#{file.split('/').last}"
+    end.take(4)
   end
 end
