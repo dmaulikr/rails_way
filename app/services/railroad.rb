7c17595f22abf18af84bc0a763088194ea693f9e
Lucas Caton <lucascaton@gmail.com>
2017-06-29 20:05:51 +1000

    Update Railroad class

--- app/services/railroad.rb
+++ app/services/railroad.rb
@@ -4,7 +4,7 @@ class Railroad
     @height = height
   end
 
-  def available_starting_points
+  def available_entrance_and_exit_positions
     [].tap do |results|
       @width.times do |index|
         results << ['U', 0,           index]
@@ -15,6 +15,26 @@ class Railroad
         results << ['L', index, 0]
         results << ['R', index, @width - 1]
       end
+    end
+  end
+
+  def blocks_permutation
+    block_images.repeated_permutation(@width * @height).to_a
+  end
+
+  def games_permutation
+    blocks_permutation.each_with_object([]) do |blocks, games|
+      available_entrance_and_exit_positions.permutation(2).each do |entrance_position, exit_position|
+        games << Game.new(blocks, width: @width, entrance_position: entrance_position, exit_position: exit_position)
+      end
+    end
+  end
+
+  private
+
+  def block_images
+    Dir[Rails.root.join 'app/assets/images/railsroads/*'].shuffle.map do |file_path|
+      Hash[file_path[/.*\/(.+).jpg/, 1].chars.each_slice(2).to_a]
     end
   end
 end
--- spec/services/railroad_spec.rb
+++ spec/services/railroad_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe Railroad do
-  describe '#available_starting_points' do
-    context '2 x 2' do
-      subject { described_class.new(2, 2) }
+  subject { described_class.new(2, 2) }
 
-      it 'return available starting points' do
-        expect(subject.available_starting_points).to match_array([
+  describe '#available_entrance_and_exit_positions' do
+    context '2 x 2' do
+      it 'returns available entrance and exit positions' do
+        expect(subject.available_entrance_and_exit_positions).to match_array([
           ['U', 0, 0], ['U', 0, 1],
           ['D', 1, 0], ['D', 1, 1],
           ['L', 0, 0], ['L', 1, 0],
@@ -16,14 +16,26 @@ RSpec.describe Railroad do
     context '2 x 3' do
       subject { described_class.new(2, 3) }
 
-      it 'return available starting points' do
-        expect(subject.available_starting_points).to match_array([
+      it 'returns available entrance and exit positions' do
+        expect(subject.available_entrance_and_exit_positions).to match_array([
           ['U', 0, 0], ['U', 0, 1],
           ['D', 2, 0], ['D', 2, 1],
           ['L', 0, 0], ['L', 1, 0], ['L', 2, 0],
           ['R', 0, 1], ['R', 1, 1], ['R', 2, 1]
         ])
       end
+    end
+  end
+
+  describe '#blocks_permutation' do
+    it 'returns an array with all possible blocks permutation' do
+      expect(subject.blocks_permutation.count).to eq(6_561)
+    end
+  end
+
+  describe '#games_permutation' do
+    it 'returns an array with all possible games permutation' do
+      expect(subject.games_permutation.count).to eq(367_416)
     end
   end
 end
